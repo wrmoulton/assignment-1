716abd75db90c8d61b8ba0bf68b6aab6f952abbf
wrmoulton <williamrmoulton@icloud.com>
2025-08-26 16:02:43 -0400

    Fixed any type issue with n and the operand type issue.

--- src/fib.ts
+++ src/fib.ts
@@ -1,10 +1,12 @@
 // util function that computes the fibonacci numbers
-export default function fibonacci(n) {
-  if (n < 0) {
+export default function fibonacci(n: number): number {
+  if (n < 0){
     return -1;
-  } else if (n == 0) {
+  }
+  else if (n === 0) {
     return 0;
-  } else if (n == 1) {
+  }
+  else if (n === 1){
     return 1;
   }
 
